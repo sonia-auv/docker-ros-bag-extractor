051b609a2bd5733654e344a6d4a26a838e52b27a
Martin Gauthier <gauthiermartin86@gmail.com>
2019-09-22 20:50:37 -0400

    Refactored get_filename

--- app/command/extract_bag.py
+++ app/command/extract_bag.py
@@ -10,22 +10,22 @@ import rosbag
 from cv_bridge import CvBridge
 
 
-def __get_bags_name(bag_folder, with_extension=False):
+def __get_filename(filepath):
+    filepath = os.path.basename(filepath)
+    filename = filepath.split(".")[0]
+
+    return filename
+
+
+def __get_bags_name(bag_folder):
     if os.path.isdir(bag_folder):
         filenames = next(os.walk(bag_folder))[2]
-        if with_extension:
-            bag_names = [f for f in filenames if len(f) > 0]
-        else:
-            bag_names = [f.split(".")[0] for f in filenames if len(f.split(".")[0]) > 0]
+        bag_names = [f.split(".")[0] for f in filenames if len(f.split(".")[0]) > 0]
 
         if len(bag_names) < 1:
-            print("Exiting...bag folder contains no bag to extract")
-            sys.exit(os.EX_OSERR)
-
+            raise SystemExit("[ERROR]:Exiting...bag folder contains no bag to extract")
         return bag_names
-
-    print("Exiting...provided bag folder does not exist")
-    sys.exit(os.EX_OSERR)
+    raise SystemExit("[ERROR]:[Exiting...provided bag folder does not exist")
 
 
 def __create_output_dirs(bag_folder, output_folder):
@@ -37,13 +37,13 @@ def __create_output_dirs(bag_folder, output_folder):
         if os.path.exists(folder):
             shutil.rmtree(folder, ignore_errors=True)
         os.mkdir(folder)
+        print(f"[INFO]:Created folder {folder}")
 
 
 def __generate_image_name(bag_file):
     uuid_name = str(uuid.uuid1())
 
-    bag_filepath = os.path.basename(bag_file)
-    bag_filename = bag_filepath.split(".")[0]
+    bag_filename = __get_filename(bag_file)
 
     uuid_formated_name = "frame_{}_{}.jpg".format(bag_filename, uuid_name)
 
@@ -61,19 +61,23 @@ def to_images(bag_folder, output_folder, topics):
     for bag_file in bags:
         for topic in topics:
             with rosbag.Bag(bag_file, "r") as bag:
-                print("Extracting images from {} on topic {}".format(bag_file, topic))
+                print("[INFO]:Extracting images from {} on topic {}".format(bag_file, topic))
                 for topic, msg, _ in bag.read_messages(topics=[topic]):
 
-                    img_name = __generate_image_name(bag_file)
-                    bag_filepath = os.path.basename(bag_file)
-                    bag_filename = bag_filepath.split(".")[0]
+                    img_filename_with_ext = __generate_image_name(bag_file)
+                    bag_filename = __get_filename(bag_file)
 
-                    extraction_path = os.path.join(output_folder, bag_filename, img_name)
+                    extraction_path = os.path.join(
+                        output_folder, bag_filename, img_filename_with_ext
+                    )
 
                     cv_img = bridge.compressed_imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
                     img_saved = cv2.imwrite(extraction_path, cv_img)
 
-                    # TODO: Handle case where result = false
+                    if not img_saved:
+                        raise SystemExit(
+                            f"[ERROR]: An error occured while saving image file {extraction_path}"
+                        )
 
-                    print("Extracted image {} to {}".format(img_name, extraction_path))
+                    print("[INFO]:Extracted image {} to {}".format(img_name, extraction_path))
